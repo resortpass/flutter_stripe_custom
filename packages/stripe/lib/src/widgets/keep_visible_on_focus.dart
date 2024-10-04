3bb6d603075ca7d834ce413bd97212c5eb386e82
Rodrigo M <rodrigo.martins@theboboapp.com>
2024-10-04 10:25:28 -0500

    RAD-10286: Updating CardField widget

---
+++ packages/stripe/lib/src/widgets/keep_visible_on_focus.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
+
+class KeepVisibleOnFocus extends StatefulWidget {
+  const KeepVisibleOnFocus({
+    required this.focusNode,
+    required this.child,
+    super.key,
+  });
+
+  final FocusNode focusNode;
+
+  final Widget child;
+
+  @override
+  State<KeepVisibleOnFocus> createState() => _KeepVisibleOnFocusState();
+}
+
+class _KeepVisibleOnFocusState extends State<KeepVisibleOnFocus>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    widget.focusNode.addListener(onFocusChanged);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+
+  void onFocusChanged() {
+    if (widget.focusNode.hasFocus && !_isObserverRegistered) {
+      WidgetsBinding.instance.addObserver(this);
+      _lastBottomViewInset = MediaQuery.of(context).viewInsets.bottom;
+      _isObserverRegistered = true;
+    } else {
+      WidgetsBinding.instance.removeObserver(this);
+      _isObserverRegistered = false;
+    }
+  }
+
+  late double _lastBottomViewInset;
+
+  bool _showOnScreenScheduled = false;
+
+  bool _isObserverRegistered = false;
+
+  void _showOnScreen() {
+    if (_showOnScreenScheduled) {
+      return;
+    }
+    _showOnScreenScheduled = true;
+    SchedulerBinding.instance.addPostFrameCallback((Duration _) {
+      _showOnScreenScheduled = false;
+      if (!mounted) return;
+      final renderObject = context.findRenderObject()! as RenderBox;
+
+      renderObject.showOnScreen(
+        // Inflate ensures that caret is not positioned directly at the edge.
+        rect: renderObject.paintBounds.inflate(20.0),
+      );
+    });
+  }
+
+  @override
+  void didChangeMetrics() {
+    final currentBottomViewInsets = MediaQuery.of(context).viewInsets.bottom;
+    if (_lastBottomViewInset != currentBottomViewInsets) {
+      if (_lastBottomViewInset < currentBottomViewInsets) {
+        // Because the metrics change signal from engine will come here every frame
+        // (on both iOS and Android). So we don't need to show caret with animation.
+        _showOnScreen();
+      }
+    }
+    _lastBottomViewInset = currentBottomViewInsets;
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    widget.focusNode.removeListener(onFocusChanged);
+    super.dispose();
+  }
+}
